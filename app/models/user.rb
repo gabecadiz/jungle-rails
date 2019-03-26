8e8e2191672a3e5c6c8d08bd6a87914c52bea90f
Gabriel Cadiz <gabecadiz@hotmail.com>
2019-03-26 02:32:40 +0000

    Revert "completed user specs"
    This reverts commit ba5e8107c8cbc21851862f35d7461babc7d12756.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,20 +7,4 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: {:case_sensitive => false}
   validates :password, presence: true, length: { minimum: 6}
-
-  # before_save :downcase_fields
-
-  # def downcase_fields
-  #   self.email.downcase!
-  # end
-
-  def self.authenticate_with_credentials(email,password)
-      #user is successfully authenticated
-    if user = User.where("LOWER(email) LIKE?", email.strip.downcase).first.try(:authenticate,password)
-      user
-    else
-      nil
-    end
-  end
-
 end
